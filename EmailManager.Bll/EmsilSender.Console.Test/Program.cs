bec7ea6dd6a89f82a7bc9d533b01e24421330ff6
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-08 01:20:35 +0400

    add all

---
+++ EmailManager.Bll/EmsilSender.Console.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailManager.Bll;
+
+namespace EmsilSender.Console.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSender emailSender=new EmailSender();
+            emailSender.SendEmail(new ContactResponseModel{Email = "vanhakobyan1996@gmail.com",CompanyName ="ISTC",Country = "Armenia",FullName = "Vanik Hakobyan"});
+        }
+    }
+}
