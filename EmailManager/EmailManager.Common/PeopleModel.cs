46e4f979119133791ffc090634a7443b9449391f
pqo1993 <pqoalla@gmail.com>
2018-12-10 19:28:28 +0400

    PeopleModel

---
+++ EmailManager/EmailManager.Common/PeopleModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailManager.Common
+{
+    class PeopleModel
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+
+        public PeopleModel()
+        {
+
+        }
+
+        public PeopleModel(string email, string phone, string country, string company,string title)
+        {
+            Email = email;
+            Phone = phone;
+            Country = country;
+            Company = company;
+            Title = title;
+        }
+    }
+}
