9ffabd5f68817c0fe5a2d745aa0242b933377244
levonaramyan <levonaramyan@gmail.com>
2018-12-10 19:29:26 +0400

    PeopleBase model class is created

---
+++ EmailManager/EmailManager.Common/PeopleBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailManager.Common
+{
+    class PeopleBase
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyWebpage { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+    }
+}
