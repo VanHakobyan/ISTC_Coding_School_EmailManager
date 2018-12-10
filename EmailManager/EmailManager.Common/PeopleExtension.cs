4fd5ee33c055b7de3983bae7196785a96bd2eaba
levonaramyan <levonaramyan@gmail.com>
2018-12-10 20:29:35 +0400

    From Asta

--- EmailManager/EmailManager.Common/PeopleExtension.cs
+++ EmailManager/EmailManager.Common/PeopleExtension.cs
@@ -11,6 +11,7 @@ namespace EmailManager.Common
         public static ContactModel GetContact(this PeopleModel people)
         {
             //Asta masta
+            // Edvin :P
             ContactModel myContact = new ContactModel
             {
                 FullName = $"{people.FirstName} {people.LastName}",
