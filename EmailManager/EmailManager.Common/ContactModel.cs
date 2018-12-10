50ea3fbe60e4bf8a9f715f0f1614b90c9e353937
vanhakobyan <vanhakobyan@gmail.com>
2018-12-10 13:37:12 +0400

    add common

---
+++ EmailManager/EmailManager.Common/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailManager.Common
+{
+    public class ContactModel
+    {
+        public string FullName { get; set; }
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ EmailManager/EmailManager.Common/Profile.cs
@@ -0,0 +1,14 @@
+﻿namespace EmailManager.Common
+{
+    public class Profile
+    {
+        public string Nickname { get; set; }
+        public string Abonnés { get; set; }
+        public string ProfilePageUrl { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string eMail { get; set; }
+        public string ContactPageURL { get; set; }
+        public string InstagramURL { get; set; }
+        public string FacebookURL { get; set; }
+    }
+}
