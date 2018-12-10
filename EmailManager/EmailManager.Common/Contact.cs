93c23103f04bcb8b05258a33eeaa27f9844c1855
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-10 19:30:23 +0400

    Add Contact Model

---
+++ EmailManager/EmailManager.Common/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailManager.Common
+{
+    class Contact
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Company { get; set; }
+        public string CompanyWebSite { get; set; }
+    }
+}
